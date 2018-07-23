0db090d67b7e33965be1be2900617dff560147e1
Chris Thomas <webmaster102@gmail.com>
2018-07-22 23:32:30 -0400

    added angular2-starter app

---
+++ angular2-starter/app/first.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  template: `
+    <h3>First Component</h3>
+    <p>This is a the content of FirstComponent</p>
+  `
+})
+export class FirstComponent {}
